166c11942abde6746369ca55597fd76f7f5cd168
rich11316 <richard.kennedy11316@gmail.com>
2018-12-16 17:59:01 +0700

    Update user.php

--- user.php
+++ user.php
@@ -7,8 +7,9 @@ $msgtoto = "[!] enjoy your life";
 $msgyd = "[!] enjoy your life";
 $msgwe = "[!] enjoy your life";
 $msgveeu = "[!] enjoy your life";
+-
 $data= [
-'adi bordir',
+'adi bordir','Raden Panji Tri Wahyudi',
  'Adi Digitizer','Dzi','semua andinya','sungging','asep kanceuh','putripesek','nubie305','ateng','Bal Lack','Maulana Setiawan','suga zora','Thauvan GT','yassar sha\'id shafi\'i','Moezza 01','စုံလင်သော အလ','hasan coc','Pro Master','hasan abut','gan bro','Yudi Pratmaja','Gembel Elite','imel telu','SkaLova','Badrun','Nhóc Con Sr','Delwin Yoanes',
 'Beuti Lavea','MasterCraft Vn','Ari Sandi','I love Cartoon','Kaboom','Robert Simangunsong','Mohamad Husn',
 'Lucy Thien','Adi Trader Demo','Mif Takhul','Feyy','I Dewa Gede Superatikadewa','Wendik','Qohwah',
@@ -25,7 +26,7 @@ $data= [
 'Agus Riyadi Riyadi','Richard Steven Try',
 'Yoviar Pauzi','Ayesha Advocate',
 //vipm
-'HARIS Denzlot','Raihan Chainago','Bejo Jo206','Bejo Rusak','Jordan Donathan','Bejo Mace','Jojo Vake','Deaso Bejo','Lungkik Cuiru','Riyan Kartika','Lusto Rusak','Jorsn Rusak','Jordwun jerdin','Curit Astjx','Hais Suis','Raden Panji Tri Wahyudi',
+'HARIS Denzlot','Raihan Chainago','Bejo Jo206','Bejo Rusak','Jordan Donathan','Bejo Mace','Jojo Vake','Deaso Bejo','Lungkik Cuiru','Riyan Kartika','Lusto Rusak','Jorsn Rusak','Jordwun jerdin','Curit Astjx','Hais Suis',
 //mpiv
 'Gem Bel','Hati Yang Terluka','Umi Kholiifah',
 'Aditiya Sarofi Putra','NOOB GAMING','phim lẻ','Jordwun lrus','gan bro','Gan Bro','bot siji',
