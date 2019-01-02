b6c288a3a8f496bf9fab04254f0bc9391447d6b0
rich11316 <richard.kennedy11316@gmail.com>
2019-01-03 03:47:11 +0700

    Update user.php

--- user.php
+++ user.php
@@ -2,8 +2,8 @@
 echo $red."[!] warning !!!".$t;
 echo $kuning."[-] ini adalah program ilegal".$t;
 echo $kuning."[-] resiko sepenuhnya di tanggung pengguna".$t.$ijo.$t;
-$ads= $putih."[÷]    official channel    [÷]".$t.$ijo."[-] adi bordir ".$putih."| atau |".$ijo." sungging [-]";
-$msgtoto = "[!] Subscribe Youtube adi bordir dan Sungging";
+$ads= $putih."[÷]    SI KAMPRET    [÷]".$t.$ijo."[-] ORANG GOBLOK ".$putih."| atau |".$ijo." ORANG TOLOL [-]";
+$msgtoto = "[!] BODOH";
 $msgyd = "[!] update [v.1.2]";
 $msgwe = "[!] enjoy your life";
 $msgveeu = "[!] enjoy your life";
@@ -11,7 +11,7 @@ $data= [
 //WAE
 'Raden Panji Tri Wahyudi','Richard Kennedy','R Panji Tri','Creature',
 //YD
-'R Panji Tri Wahyudi','Charles','Richard The Lionheart','richard.thesteam11316@gmail.com',
+'R Panji Tri Wahyudi','Charles','Richard The Lionheart','richard.thesteam11316@gmail.com','richard.arima11316@gmail.com'
 //mpiv
         ];
 $toto = ['tes',];
